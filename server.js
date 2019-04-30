9a4a0323be10394b4ca6c72ae936f54163de3895
yitzhak-bloy <41252020+yitzhak-bloy@users.noreply.github.com>
2019-04-30 12:43:35 +0300

    Build the endpoint of the register

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ const database = {
 }
 
 app.get('/', (req, res) => {
-    res.send('this is working');
+    res.send(database.users);
 })
 
 app.post('/signin', (req, res) => {
@@ -37,6 +37,19 @@ app.post('/signin', (req, res) => {
     } else {
         res.status(400).json('הפעולה נכשלה')
     }
+})
+
+app.post('/register', (req, res) =>{
+    const {email, name, password } = req.body;
+    database.users.push({
+        id: '125',
+        name: name,
+        email: email,
+        password: password,
+        entries: 0,
+        joined: new Date()
+    })
+    res.json(database.users[database.users.length-1]);
 })
 
 app.listen(3000, ()=> {
